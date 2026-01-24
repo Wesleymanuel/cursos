d7a1ab0672ecf82f46e14f3ca4f977efdf5a6c05
FernandoVini <wesleyemanuel0510@gmail.com>
2026-01-24 19:33:10 -0300

    feat(cards): add course cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import Box  from './components/Box'
 import './App.css'
+import { GrCertificate } from "react-icons/gr";
+import { FaStar } from "react-icons/fa";
+import { FaRobot } from "react-icons/fa";
 import LangingPage from './components/LangingPage'
+import CursesCardRoot from './components/card/CursesCardRoot';
 
 function App() {
 
   return (
     <>
       <LangingPage/>
+      <div className='boxs-root'>
+        <Box icon={FaStar} text='best reviews'/>
+        <Box icon={GrCertificate} text='international certifications'/>
+        <Box icon={FaRobot} text='innovations with AI'/>
+      </div>
+      <div className='cards-root'>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+      </div>
+      <div className='cards-root'>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+        <CursesCardRoot/>
+      </div>
     </>
   )
 }
---
+++ src/components/card/CursesCardRoot.tsx
@@ -0,0 +1,33 @@
+import './CursesCards.css'
+
+const imagem = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpg8Sl78R9oIwILZiEkpzKYXHAI0Z60uoI4g&s"
+
+const CursesCardRoot = () => {
+  return (
+    <main className='cards-main'>
+        <div className='img-container' style={{ backgroundImage: `url(${imagem})`}}>
+        </div>
+        <div className='titulo'>
+            <p>titulo</p>
+        </div>
+        <div className='especificoes'>
+            <div>
+                <p>top vendas</p>
+            </div>
+            <div>
+                <p>certificado</p>
+            </div>
+            <div>
+                <p>espeficao</p>
+            </div>
+        </div>
+        <div className='preco'>
+            <p>preco</p>
+        </div>
+    </main>
+  )
+}
+
+export default CursesCardRoot
+
+{/*"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpg8Sl78R9oIwILZiEkpzKYXHAI0Z60uoI4g&s" */}
