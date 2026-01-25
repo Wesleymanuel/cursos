83c34ca80bea2bd78068824973a33ffa03928bc1
FernandoVini <wesleyemanuel0510@gmail.com>
2026-01-24 21:21:52 -0300

    fix(header): fix header buttons bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,15 @@ import { Button } from '@mantine/core';
 import { FaMeteor } from "react-icons/fa";
 import Input from './Input';
 import SelectComponent from './SelectComponent';
+import { useMediaQuery } from '@mantine/hooks';
+import { useDisclosure } from '@mantine/hooks';
+import { Burger } from '@mantine/core';
 
 const Header = () => {
+
+    const media = useMediaQuery('(max-width: 660px)')
+    const [opened, { toggle }] = useDisclosure();
+
   return (
     <header className='header'>
         <nav className='header-nav'>
@@ -16,11 +23,22 @@ const Header = () => {
                 <li><SelectComponent/></li>
                 <li><SelectComponent/></li>
             </ul>
-            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                <Input/>
-                <Button variant="transparent">
-                    sing in
-                </Button>
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}} className='sla1'>
+                <div >
+                    <Input/>
+                </div>
+                {media ? (
+                    <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    aria-label="Toggle navigation"
+                    color="#1c73be"
+                    />
+                ) : (
+                    <Button variant="transparent" className='button-input'>
+                    Sign in
+                    </Button>
+                )}
             </div>
         </nav>
     </header>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,11 @@
 import './Input.css'
 import { FaSearch } from "react-icons/fa";
+import { useMediaQuery } from '@mantine/hooks';
 
 const Input = () => {
+
+  const mediaQuery = useMediaQuery('max-width: ')
+
   return (
     <main className='input-main'>
         <div className='search-logo'> 
